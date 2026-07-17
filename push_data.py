84bb7e543239e1f74443c4d27287e0c233072b75
srinjoy2006eb-glitch <srinjoy2006eb@gmail.com>
2026-07-17 19:09:42 +0530

    setup updated

--- push_data.py
+++ push_data.py
@@ -6,42 +6,75 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_DB_URL = os.getenv("MONGO_DB_URL")
-print(MONGO_DB_URL)
 
 import certifi
 ca = certifi.where()
 
 import pandas as pd
-from pymongo import MongoClient 
-import numpy as np
+from pymongo import MongoClient
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logger
 
-class NetworkDataExtract():
+
+class NetworkDataExtract:
     def __init__(self):
         try:
             pass
         except Exception as e:
             raise NetworkSecurityException(e, sys) from e
 
-    def cv_to_json(self,file_path):
+    def csv_to_json(self, file_path):
+        """
+        Converts a CSV file into a list of JSON records.
+        """
         try:
             data = pd.read_csv(file_path)
             data.reset_index(drop=True, inplace=True)
-            records=list(json.loads(data.T.to_json()).values())
-            return records
-        except Exception as e:
-        
-            raise NetworkSecurityException(e, sys) 
-    
 
-    def insert_data_mongodb(self,records,database,collection):
-        try:
-            self.database = database
-            self.collection = collection
-            self.records = records
-            self.mongo_client = MongoClient(MONGO_DB_URL,)
-            self.collection.insert_many(self.records)
-            return(len(self.records))
+            records = list(json.loads(data.T.to_json()).values())
+
+            return records
+
         except Exception as e:
-            raise NetworkSecurityException(e, sys)
+            raise NetworkSecurityException(e, sys) from e
+
+    def insert_data_mongodb(self, records, database, collection):
+        """
+        Inserts records into MongoDB.
+        """
+        try:
+            self.mongo_client = MongoClient(
+                MONGO_DB_URL,
+                tlsCAFile=ca
+            )
+
+            self.database = self.mongo_client[database]
+            self.collection = self.database[collection]
+
+            self.collection.insert_many(records)
+
+            logger.info(f"{len(records)} records inserted into MongoDB.")
+
+            return len(records)
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys) from e
+
+
+if __name__ == "__main__":
+    FILE_PATH = "Network_Data/phisingData.csv"
+    DATABASE = "NetworkSecurity"
+    COLLECTION = "NetworkData"
+
+    network_obj = NetworkDataExtract()
+
+    records = network_obj.csv_to_json(FILE_PATH)
+
+    no_of_records = network_obj.insert_data_mongodb(
+        records,
+        DATABASE,
+        COLLECTION
+    )
+
+    print(f"{no_of_records} records inserted successfully.")
--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 from typing import List
-def
+
+def get_requirements() -> List[str]:
+    requirements_lst = []
+
+    try:
+        with open("requirements.txt", "r") as file:
+            lines = file.readlines()
+
+            for line in lines:
+                requirement = line.strip()
+
+                if requirement and requirement != "-e .":
+                    requirements_lst.append(requirement)
+
+    except FileNotFoundError:
+        print("requirements.txt file not found.")
+
+    return requirements_lst
+
+setup(
+    name="network_security",
+    version="0.0.1",
+    author="Anitra Boral",
+    author_email="anitra.boral@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
